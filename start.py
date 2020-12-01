071842ae7898fa8f359a696289fe4847f0057d82
Baby <69705110+BabyxD@users.noreply.github.com>
2020-12-01 08:13:50 +0530

    Update start.py

--- start.py
+++ start.py
@@ -9,7 +9,7 @@ client = TelegramClient('anfghohn', int(os.environ.get("APP_ID" )), os.environ.g
 @client.on(events.NewMessage(pattern='/start'))
 async def handler(event):
     chat = await event.get_chat()
-    await client.send_message(chat,"""💁 <b>HELLO 💙 Decoded @Baby_xD</b> \n\n🔗 Send Me: Zee5 & MX player Movies links, I will give there 👩‍💻 streaming links. \n⭕️ Mx Players link format: \n\n✅ https://www.mxplayer.in/movie/039664d4d85c603cfb5a6cd66b9e29ec \n\n ❌ https://www.mxplayer.in/movie/xzy-name-movie-039664d4d85c603cfb5a6cd66b9e29ec
+    await client.send_message(chat,"""💁 HELLO 💙 Decoded @Baby_xD \n\n🔗 Send Me: Zee5 & MX player Movies links, I will give there 👩‍💻 streaming links. \n\n⭕️ Mx Players link format: \n\n✅ https://www.mxplayer.in/movie/039664d4d85c603cfb5a6cd66b9e29ec \n\n ❌ https://www.mxplayer.in/movie/xzy-name-movie-039664d4d85c603cfb5a6cd66b9e29ec
  """)
     
 
